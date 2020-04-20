280ab0c0a5258b5cb8429e7a45c6b3f3daaa7163
Luigi Fontana <luigifontana92@gmail.com>
2020-04-20 16:01:49 +0200

    Creazione data.php

---
+++ data.php
@@ -0,0 +1,64 @@
+<?php
+    $discs = [
+        [
+            'cover' => 'img/cover1.jpg',
+            'title' => 'Elvis Presley',
+            'author' => 'Elvis Presley',
+            'year' => '1956'
+        ],
+        [
+            'cover' => 'img/cover2.jpg',
+            'title' => 'Sgt Pepper’s Lonely Hearts Club Band',
+            'author' => 'The Beatles',
+            'year' => '1967'
+        ],
+        [
+            'cover' => 'img/cover3.jpg',
+            'title' => 'The Velvet Underground & Nico',
+            'author' => 'The Velvet Underground',
+            'year' => '1967'
+        ],
+        [
+            'cover' => 'img/cover4.jpg',
+            'title' => 'Dark Side of the Moon',
+            'author' => 'Pink Floyd',
+            'year' => '1973'
+        ],
+        [
+            'cover' => 'img/cover5.jpg',
+            'title' => 'Never Mind The Bollocks',
+            'author' => 'Sex Pistols',
+            'year' => '1977'
+        ],
+        [
+            'cover' => 'img/cover6.jpg',
+            'title' => 'Unknown Pleasures',
+            'author' => 'Joy Division',
+            'year' => '1979'
+        ],
+        [
+            'cover' => 'img/cover7.jpg',
+            'title' => 'Rio',
+            'author' => 'Duran Duran',
+            'year' => '1982'
+        ],
+        [
+            'cover' => 'img/cover8.jpg',
+            'title' => 'War',
+            'author' => 'U2',
+            'year' => '1983'
+        ],
+        [
+            'cover' => 'img/cover9.jpg',
+            'title' => 'Licensed to Ill',
+            'author' => 'Beastie Boys',
+            'year' => '1986'
+        ],
+        [
+            'cover' => 'img/cover10.jpg',
+            'title' => 'Fear of a Black Planet',
+            'author' => 'Public Enemy',
+            'year' => '1990'
+        ]
+    ];
+?>
